2906ae3e2c94b778a91bfbb502b3b24355aced4e
jsbean <jbean46@gmail.com>
2016-03-11 15:36:17 -0500

    Update docs with new gen script

---
+++ GenerateDocumentation.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cd ../
+cd Frameworks
+
+for i in $( ls ); do
+  if [[ -d $i ]]; then
+    if ! [[ $i = dependencies ]]; then
+      
+      cd $i
+
+      VERSION=$(git describe --tags | cut -d - -f -1)
+      echo $i: $VERSION
+
+      jazzy \
+        --clean \
+        --author James Bean \
+        --author_url http://jamesbean.info \
+        --github_url https://github.com/dn-m/$i \
+        --module-version $VERSION \
+        --module $i \
+        --root-url https://dn-m.github.io \
+        --output ../../site/$i \
+        --skip-undocumented \
+        --hide-documentation-coverage \
+        --theme fullwidth
+      cd ../
+    fi
+  fi
+done
+
+cd ../
+cd site
